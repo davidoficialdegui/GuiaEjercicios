3075e7d2c5ff7b97b6004d7cb1b77a751dcd9d7e
David Oficialdegui <david.oficialdegui@opendeusto.es>
2024-10-16 20:11:08 +0200

    Ejercicio 2

--- GuiaEjercicios/src/gui/main/MainWindow.java
+++ GuiaEjercicios/src/gui/main/MainWindow.java
@@ -1,6 +1,20 @@
 package gui.main;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+
+import domain.Athlete;
+import domain.Athlete.Genre;
 
 public class MainWindow extends JFrame {
 
@@ -13,9 +27,51 @@ public class MainWindow extends JFrame {
 		setTitle("JJ.OO. París 2024");
 		setSize(640, 480);
 		
-		setLocationRelativeTo(null);
+		setLocationRelativeTo(null);	
+		
+		
+		
+		//JList
+		
+		ArrayList<String> atletas = new ArrayList<String>();
+		
+		int i;
+		
+		for( i = 1 ;i < 41; i++ ) {
+			atletas.add("atleta " + i);
+		}
+		
+		
+		DefaultListModel<String> modeloJlist = new DefaultListModel<String>();  
+		modeloJlist.addAll(atletas);
+		
+		JList<String> jList = new JList<String>(modeloJlist); 
+		jList.setFixedCellWidth(200); 
+		
+		JScrollPane jScrollPane = new JScrollPane(jList); 
+		
+		add(jScrollPane, BorderLayout.WEST);  
+		
+		
+		
+		// JTabbed
+		
+		JTabbedPane panelTabbed = new JTabbedPane();
+		
+		JPanel Tab1 = new JPanel();
+		JPanel Tab2 = new JPanel();
+		
+		panelTabbed.addTab("Datos", Tab1);
+		panelTabbed.addTab("Medallas", Tab2);
+		
+		add(panelTabbed, BorderLayout.CENTER);
+		
+		
+		
 		
 		setVisible(true); 
+		
+		
 		
 	}
 	
