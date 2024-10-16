0056a99c80d281415331dca131a1eecee111e180
David Oficialdegui <david.oficialdegui@opendeusto.es>
2024-10-16 08:51:02 +0200

    Ejercicio 1

---
+++ GuiaEjercicios/src/gui/main/MainWindow.java
@@ -0,0 +1,24 @@
+package gui.main;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public MainWindow () {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+		setTitle("JJ.OO. París 2024");
+		setSize(640, 480);
+		
+		setLocationRelativeTo(null);
+		
+		setVisible(true); 
+		
+	}
+	
+	
+
+}
